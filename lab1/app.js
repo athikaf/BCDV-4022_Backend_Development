781ef12407cd77e6bdde8e9bb720fdcc39db2468
Athika Fatima <athikafatima16@gmail.com>
2023-10-31 20:29:59 -0400

    Successfully executed app.js

--- lab1/app.js
+++ lab1/app.js
@@ -2,14 +2,17 @@ const fsModule = require("./modules/fileSystem");
 const eventEmitterModule = require("./modules/eventEmitter");
 const cryptoModule = require("./modules/crypto");
 
-fsModule.writeToFile("example.txt", "Hello, File System!");
-const content = fsModule.readFromFile("example.txt");
+fsModule.writeToFile(
+  "athikaFatima.txt",
+  "Hello, File System! This is Athika Fatima - 101502209"
+);
+const content = fsModule.readFromFile("athikaFatima.txt");
 console.log(`File content: ${content}`);
 
 eventEmitterModule.listenForCustomEvent((message) => {
   console.log(`Received custom event: ${message}`);
 });
-eventEmitterModule.emitCustomEvent("Hello, NodeJS!");
+eventEmitterModule.emitCustomEvent("Hello, NodeJS! This is Athika Fatima!");
 
 const hashedPassword = cryptoModule.hashPassword("MySecurePassword");
 console.log(`Hashed password: ${hashedPassword}`);
