18bdf2579485d215d76f4e35dd3209b39de2a552
Athika Fatima <athikafatima16@gmail.com>
2023-10-31 20:16:57 -0400

    Added data in the files inside module folder

---
+++ lab1/modules/fileSystem.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+
+function writeToFile(filename, content) {
+  fs.writeFileSync(filename, content);
+}
+
+function readFromFile(filename) {
+  return fs.readFileSync(filename, "utf-8");
+}
+
+module.exports = {
+  writeToFile,
+  readFromFile,
+};
