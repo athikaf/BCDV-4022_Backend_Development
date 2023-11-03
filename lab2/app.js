b256dfbfb92475f988baa892da5ba2303b5e1588
Athika Fatima <athikafatima16@gmail.com>
2023-11-02 20:23:30 -0400

    Added data to app.js and created tasks.js

---
+++ lab2/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.json());
+
+const taskRouter = require("./tasks");
+
+app.use("/tasks", tasksRouter);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
