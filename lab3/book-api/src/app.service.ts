74ced46821955afcdaa411576fa11dd727329370
Athika Fatima <athikafatima16@gmail.com>
2023-11-05 19:05:41 -0500

    Completed Lab3 assignment

--- lab3/book-api/src/app.service.ts
+++ lab3/book-api/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World! This Nest Project is by Athika Fatima - 101502209';
   }
 }
